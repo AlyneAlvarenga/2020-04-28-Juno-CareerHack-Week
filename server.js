83df4ce068b5637fe442c4f742653ac1f1e048b4
AlyneAlvarenga <alynevs@gmail.com>
2020-04-28 12:42:30 -0400

    server is running and index.html created

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+//register static middleware (between front and backend, in the middle of two pieces of code)
+const staticMiddelware = express.static('public')
+app.use(staticMiddelware);
+
+let numberofClicks = 0;
+
+//changed route because want index.html to be served from the root
+app.get('/api/json', (req, res) => {
+  const json = {};
+  json.message = "Hello World Againnnn"
+  json.otherMessage = 'Huh'
+  json.numberofClicks = numberofClicks;
+  res.json(json);
+});
+
+app.post('/api/json', (req, res) => {
+  numberofClicks++;
+  res.redirect('/');
 })
 
 app.listen(3000, () => {
